5aa658359854e860b7f0bfa8811eaa2aa688fad5
Rutro187 <mjohnson@esomaha.com>
2019-08-27 16:50:01 -0500

    das

--- Dmail/server/models/User.js
+++ Dmail/server/models/User.js
@@ -1,8 +1,8 @@
 let mysql = require('mysql');
-let pool= require('./connections.js');
+let pool= require('../connections.js');
  
 
-let SQL = `CALL login`;
+let SQL = `CALL login(?)`;
 
 pool.query(SQL, true, (error, results,) => {
     if (error) {
